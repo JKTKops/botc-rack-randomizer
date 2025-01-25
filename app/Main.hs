0c31a15550ff6c68ddd8b1c148db288f8fd7212c
Max Kopinsky <zerglingk9012@gmail.com>
2025-01-25 01:32:26 -0500

    simple frontend

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,24 @@
 module Main (main) where
 
+import Control.Monad
+import System.Environment
+import System.Exit (exitFailure)
+
+import Randomizer (rack)
+import Script (loadScript)
+
 main :: IO ()
-main = putStrLn "Hello, Randomizer!"
+main = do
+  args <- getArgs
+  when (length args /= 2) $ printUsage >> exitFailure
+  let [path, pcs] = args
+      pc = read pcs
+  script <- loadScript path
+  rack script pc
+
+printUsage :: IO ()
+printUsage = putStrLn $ unlines
+  [ "Usage: rack-botc SCRIPTFILE PC"
+  , "  SCRIPTFILE\t\tthe path to a suitable .json file for the script"
+  , "  PC        \t\tthe playercount to rack for"
+  ]
